17093f5614f4368d165891bba6fa06892a55d58e
Rogerio de Oliveira <58958893+Rogerio-mack@users.noreply.github.com>
2023-03-13 12:28:46 -0300

    Add files via upload

---
+++ exemplo1.sql
@@ -0,0 +1,8 @@
+--
+-- questão 1
+-- 
+use world;
+--
+-- questão 2
+--
+show tables;
---
+++ exemplo2.sql
@@ -0,0 +1,34 @@
+--
+-- Create database
+--
+CREATE DATABASE alunos;
+
+--
+-- Change database
+--
+USE alunos;
+
+--
+-- Create Table
+--
+CREATE TABLE Persons (
+    ID int NOT NULL,
+    LastName varchar(255) NOT NULL,
+    FirstName varchar(255),
+    Age int,
+    PRIMARY KEY (ID)
+);
+
+--
+-- Populate Table
+--
+INSERT INTO Persons (ID, LastName, FirstName, Age) 
+VALUES ('1', 'Einstein', 'Albert', 65);
+
+INSERT INTO Persons (ID, LastName, FirstName, Age) 
+VALUES ('2', 'Joyce', 'James', 60);
+
+--
+-- Select Table
+--
+SELECT * FROM Persons;
