e0d357933b453a81b4c31f1d8e29ea4330866fa0
Rogerio de Oliveira <58958893+Rogerio-mack@users.noreply.github.com>
2024-05-21 09:28:07 -0300

    Create picachu.sql

---
+++ picachu.sql
@@ -0,0 +1,29 @@
+/*
+5. Mostre uma lista de todos os diferentes valores de ContactTitle em Customers e suas respectivas quantidades. Exiba esses valores em ordem decrescente das quantidades.
+*/
+
+
+/*
+6. Mostre para cada produto, o Fornecedor (Supplier) associado. Mostre o ProductID, ProductName e o CompanyName do fornecedor. Ordene por ProductID. 
+*/
+
+
+/*
+7. Gostaríamos de mostrar uma lista dos Pedidos que foram feitos, incluindo o Expedidor (Shipper) que foi usado. Mostre o OrderID, OrderDate (somente data) e CompanyName do Expedidor e classifique por OrderID. Para não mostrar todos os pedidos (há mais de 800), mostre apenas as linhas com um OrderID menor que 10300.
+*/
+
+
+/*
+7B. Liste os nomes dos produtos que possuem um preço maior do que a média dos preços dos produtos.
+*/
+
+
+/*
+7C. Liste os clientes que nunca fizeram um pedido EMPREGANDO SUB SELECT no lugar de JOIN!
+*/
+
+
+/*
+7D. Liste os produtos que foram vendidos mais de 100 unidades em qualquer pedido.
+*/
+
