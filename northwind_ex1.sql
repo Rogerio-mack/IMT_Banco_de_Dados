b726830f0263554b72f5d2871200293230aa9823
Rogerio de Oliveira <58958893+Rogerio-mack@users.noreply.github.com>
2024-05-21 07:56:52 -0300

    Create northwind_ex1.sql

---
+++ northwind_ex1.sql
@@ -0,0 +1,28 @@
+/* 
+Problemas Básicos
+*/
+
+/* 
+1. Na tabela OrderDetails, temos os campos UnitPrice e Quantity. Crie um novo campo, TotalPrice e retorne também OrderID, ProductID, UnitPrice e Quantity. Ordene por OrderID e ProductID.
+*/ 
+
+/*
+2. Quantos clientes temos na tabela Clientes?
+*/
+
+/*
+3. Mostre a data do primeiro pedido já feito na tabela Pedidos.
+*/
+
+/*
+4. Mostre uma lista de países onde a empresa Northwind tem clientes.
+*/
+
+/*
+4B. Liste a quantidade de pedidos feitos por cada funcionário.
+*/
+
+/*
+4C. Liste o total de vendas (valor total) por cada cliente e ordene por nome de Companyname.
+*/
+
