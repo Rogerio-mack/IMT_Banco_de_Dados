477715afb739d789351137aeda0b060c23163dbe
Rogerio de Oliveira <58958893+Rogerio-mack@users.noreply.github.com>
2024-05-28 08:58:24 -0300

    Create northwind_ex3.sql

---
+++ northwind_ex3.sql
@@ -0,0 +1,48 @@
+/*
+7E.  Liste os IDs dos funcionários que fizeram mais de 50 pedidos. (Como ficaria com o nome deles?)
+*/
+
+/* 
+7F. Liste os produtos que tiveram um total de vendas (quantidade) superior a 500.
+*/
+
+/*
+Problemas Intermediários
+*/
+
+/*
+8. Para este problema, gostaríamos de ver o número total de produtos em cada categoria. Classifique os resultados pelo número total de produtos, em ordem decrescente.
+*/
+
+/*
+9. Na tabela Clientes, mostre o número total de clientes por País e Cidade.
+*/
+
+/*
+10. Alguns dos países da base de dados têm taxas de frete muito altas. Gostaríamos de investigar mais algumas opções de envio para nossos clientes, para poder oferecer-lhes taxas de frete mais baixas. Retorne os três países de navio com o maior frete médio geral, em ordem decrescente por frete médio.
+*/
+
+/*
+11. Continuamos na questão acima sobre as altas taxas de frete. Agora, em vez de usar todos os pedidos que temos, queremos apenas ver pedidos do ano de 2015.
+*/
+
+/*
+12. Continuamos trabalhando com altas taxas de frete. Queremos agora obter os três países de navios com as taxas de frete médias mais elevadas. Mas, em vez de filtrar para um determinado ano, queremos usar os últimos 12 meses de dados do pedido, usando como data de término o último OrderDate em Pedidos.
+*/
+
+/*
+13. Há alguns clientes que nunca fizeram um pedido. Mostre a esses clientes.
+*/
+
+/*
+14. Você quer enviar a todos os clientes de alto valor um presente VIP especial. Vamos definir clientes de alto valor como aqueles que fizeram pelo menos 1 pedido com um valor total (sem incluir o desconto) igual a US$ 10.000 ou mais. Queremos considerar apenas os pedidos feitos no ano de 2016.
+*/
+
+/*
+15. Uma das vendedoras, veio até você com um pedido. Ela acha que acidentalmente inseriu duas vezes um item de linha em um pedido, com um ProductID diferente, mas a mesma quantidade. Ela lembra que a quantidade era de 60 ou mais. Mostre todos os OrderIDs com itens de linha que correspondam a isso, na ordem de OrderID.
+*/
+
+/*
+16. Com base na pergunta anterior, queremos agora mostrar detalhes do pedido, para pedidos que correspondem aos critérios acima.
+*/
+
