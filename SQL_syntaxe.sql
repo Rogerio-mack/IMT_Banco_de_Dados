0cbce9c460748d866aeebad3599f2183531cf0e2
Rogerio de Oliveira <58958893+Rogerio-mack@users.noreply.github.com>
2025-05-26 10:32:10 -0300

    Create SQL_syntaxe.sql

---
+++ SQL_syntaxe.sql
@@ -0,0 +1,16 @@
+SELECT
+    [ALL | DISTINCT]
+
+    select_expr [, select_expr] ...
+    
+  [FROM table_references
+    [JOIN [ ON join_condition | USING(field) ]]
+    [WHERE where_condition]
+
+    [GROUP BY {col_name | expr | position}]
+    [HAVING having_condition]
+    
+    [ORDER BY {col_name | expr | position}
+      [ASC | DESC]
+
+
